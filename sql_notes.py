4f32bdf95ff2df6a043c404c4ffc2f3b78f2bc0f
lokeshwari1119 <lokeshwari.tech1119@gmail>
2026-09-13 10:21:15 +0530

    changed number to integer

--- sql_notes.py
+++ sql_notes.py
@@ -1,3 +1,3 @@
-a=int(input("Enter a number: "))
-b=int(input("Enter another number: "))
-print("The sum of the two numbers is:", a + b)
+a=int(input("Enter an integer: "))
+b=int(input("Enter another integer: "))
+print("The sum of the two integers is:", a + b)
